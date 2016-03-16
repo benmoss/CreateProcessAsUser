43609ca6c08497856503b576f4e9afafbc5e6d8f
Ben Moss <bmoss@pivotal.io>
2016-03-16 10:42:50 -0400

    Clean up whitepsace

--- RunProcessAsUser/Program.cs
+++ RunProcessAsUser/Program.cs
@@ -11,9 +11,7 @@ namespace RunProcessAsUser
     {
         static void Main(string[] args)
         {
-            //NativeMethods.LaunchChildProcess(new NetworkCredential("greenhouse", "cat9lives"), "dir");
-            //murrayju.ProcessExtensions.ProcessExtensions.StartProcessAsCurrentUser(@"C:\Windows\System32\cmd.exe");
-            CreateProcessSample.Win32.LaunchCommand2("ping -t 127.0.0.1", ".", "foobar", "foobar");
+            CreateProcessSample.Win32.CreateProcessAsUserWrapper("ping -t 127.0.0.1", ".", "foobar", "foobar");
         }
     }
 }
